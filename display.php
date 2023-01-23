3c7f4e1147f1170e0bb993a996a7fb4c1ade28cc
Sai <sai081118@gmail.comgit commit>
2023-01-23 15:01:47 +0630

    die error check

--- display.php
+++ display.php
@@ -66,7 +66,7 @@ $response = mysqli_query($connectdb, $query);
         </tbody>";
             }
         } else {
-            echo "Nodata";
+            die($mysqli_error($connectdb));
         }
         ?>
     </table>
